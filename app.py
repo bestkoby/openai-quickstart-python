2cf789c94abbdbd6629dcf2aba654eeafdae3183
bestkoby <kl780@scarletmail.rutgers.edu>
2023-02-10 18:30:11 -0500

    add image

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ import os
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
+from werkzeug.utils import secure_filename
 
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static')
 
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -15,29 +20,37 @@ def allowed_file(filename):
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        file = request.files['file']
-        if file and allowed_file(file.filename):
-            return ###
-        response = openai.Image.create(
-            prompt="a white siamese cat",
-            n=1,
-            size="1024x1024"
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
-
+        if request.form["action"] == "Generate images":
+            prompt = request.form["prompt"]
+            response = openai.Image.create(
+                prompt=prompt,
+                n=1,
+                size="1024x1024"
+            )
+            image_url = response["data"][0]["url"]
+            return redirect(url_for("index", result=image_url))
+        elif request.form["action"] == "Variation":
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                print('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename == '':
+                print('No selected file')
+                return redirect(request.url)
+            filename = secure_filename(file.filename)
+            print(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+            if file and allowed_file(file.filename):
+                response = openai.Image.create_variation(
+                    image=open(os.path.join(app.config['UPLOAD_FOLDER'],filename), "rb"),
+                    n=1,
+                    size="1024x1024"
+                )
+                image_url = response['data'][0]['url']
+                return redirect(url_for("index", result=image_url))
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
